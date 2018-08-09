dbb8445978a6c36e0bc71fe090a347a4227f4bc8
Thea2 <1822947065@qq.com>
2018-08-09 09:25:50 +0800

    第四十九篇

---
+++ src/Offer49.java
@@ -0,0 +1,30 @@
+/**
+ * 题目描述：将一个字符串转换成一个整数(实现Integer.valueOf(string)的功能，
+ *          但是string不符合数字要求时返回0)，要求不能使用字符串转换整数的库函数。
+ *          数值为0或者字符串不是一个合法的数值则返回0。
+ *
+ * 解题思路：数字0-9对应字符48-57，先将字符串转换为字符数组再一一对比
+ */
+
+public class Offer49 {
+
+    public int StrToInt(String str) {
+        if (str.equals("") || str.trim().length() == 0){
+            return 0;
+        }
+        char[] a = str.toCharArray();
+        int fuhao = 0;
+        if (a[0] == '-')
+            fuhao = 1;
+        int sum = 0;
+        for (int i = fuhao; i < a.length; i++)
+        {
+            if (a[i] == '+')
+                continue;
+            if (a[i] < 48 || a[i] > 57)
+                return 0;
+            sum = sum * 10 + a[i] - 48;
+        }
+        return fuhao == 0 ? sum : sum * -1;
+    }
+}
